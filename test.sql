331613a67e9cddfdc56b1d37279e7370a2a38363
Mattreed091 <reedmatthew38@gmail.com>
2025-01-16 16:48:46 -0500

    test.sql Update 0.0.1

---
+++ test.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE geeks;
+USE geeks;
+CREATE TABLE demo_table(
+NAME VARCHAR(20),
+AGE int,
+GRADE VARCHAR(5));
+INSERT INTO demo_table
+VALUES ('Romy',22, 'A'),
+('Roshini', 20, 'A'),
+('Akanksha', 22, 'A'),
+('Shalini',23,'B'),
+('Sambhavi',21,'A'),
+('Meenakshi',22,'C'),
+('Nikhil',24,'B'),
+('Manu',25,'B'),
+('Rohit',24,'C'),
+('Astha',23,'A'),
+('Samiksha',23,'A');
